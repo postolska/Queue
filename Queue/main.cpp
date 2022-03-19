975effa95de2f8457f3e23b909de86426272f250
Paulina Ostolska <pa.ostolska@gmail.com>
2022-03-19 16:20:44 +0100

    Initial Commit

---
+++ Queue/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Queue
+//
+//  Created by Paula Ostolska on 19/03/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
